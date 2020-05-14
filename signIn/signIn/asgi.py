dabcf9aaf268d53357af628ba88c7f89cbdc00fe
nafizzzzzzzzzz <nhremon8181@gmail.com>
2020-05-15 04:44:52 +0600

    Dashboard incomplete

---
+++ signIn/signIn/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for signIn project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.0/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'signIn.settings')
+
+application = get_asgi_application()
