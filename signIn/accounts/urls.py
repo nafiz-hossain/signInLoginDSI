2117cac14dcbec238bd945e9b726240c32d8da6d
nafizzzzzzzzzz <nhremon8181@gmail.com>
2020-05-15 08:02:03 +0600

    Initial done login signIn

--- signIn/accounts/urls.py
+++ signIn/accounts/urls.py
@@ -2,10 +2,11 @@ from django.urls import path
 from . import views
 from django.contrib.auth.views import LoginView,LogoutView
 urlpatterns = [
-    path('',views.indexView, name="home"),
+    path('', views.indexView, name="home"),
     path('dashboard/', views.dashboardView, name="dashboard"),
-    path('login/',LoginView.as_view(),name="login_url"),
+    path('base/', views.baseView, name="base"),
+    path('login/', LoginView.as_view(), name="login_url"),
     path('register/', views.registerView, name="register_url"),
-    path('logout/', LogoutView.as_view(next_page='dashboard'), name="logout"),
+    path('logout/', LogoutView.as_view(next_page='base'), name="logout"),
 
 ]
--- signIn/accounts/views.py
+++ signIn/accounts/views.py
@@ -4,6 +4,9 @@ from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 
+def baseView(request):
+    return render(request, 'login2.html')
+
 def indexView(request):
     return render(request, 'index.html')
 
